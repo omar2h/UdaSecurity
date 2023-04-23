ec68cf54b3f094ac0fbb7c434a2cddd779fe277f
omarsamak <omarhesham83@gmail.com>
2023-04-23 19:36:37 +0200

    test: if alarm is armed and sensor activated

---
+++ Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
@@ -0,0 +1,66 @@
+package com.udacity.catpoint.security.service;
+
+import com.udacity.catpoint.image.service.FakeImageService;
+import com.udacity.catpoint.security.data.AlarmStatus;
+import com.udacity.catpoint.security.data.ArmingStatus;
+import com.udacity.catpoint.security.data.SecurityRepository;
+import com.udacity.catpoint.security.data.Sensor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SecurityServiceTest {
+
+    private SecurityService securityService;
+    @Mock
+    private FakeImageService imageService;
+    @Mock
+    private SecurityRepository securityRepository;
+    @Mock
+    private Sensor sensor;
+    @BeforeEach
+    void init() {
+        securityService = new SecurityService(securityRepository, imageService);
+    }
+
+    // If alarm is armed and a sensor becomes activated, put the system into pending alarm status.
+    @ParameterizedTest
+    @MethodSource("differentArmingStatus")
+    public void changeSensorActivationStatus_alarmArmedAndSensorActivated_changedSystemIntoPendingAlarmStatus(
+            ArmingStatus armingStatus){
+        when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+        when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
+        securityService.changeSensorActivationStatus(sensor, true);
+        verify(securityRepository, times(1)).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+    }
+
+    // If alarm is armed and a sensor becomes activated and the system is already pending alarm, set the alarm status to alarm.
+    @ParameterizedTest
+    @MethodSource("differentArmingStatus")
+    public void changeSensorActivationStatus_alarmArmedAndSensorActivatedAndSystemAlarmPending_changedSystemIntoAlarmStatus(
+            ArmingStatus armingStatus){
+        when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+        when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+        securityService.changeSensorActivationStatus(sensor, true);
+        verify(securityRepository, times(1)).setAlarmStatus(AlarmStatus.ALARM);
+    }
+
+    private static Stream<Arguments> differentArmingStatus() {
+        return Stream.of(
+                Arguments.of(ArmingStatus.ARMED_AWAY),
+                Arguments.of(ArmingStatus.ARMED_HOME)
+        );
+    }
+}
